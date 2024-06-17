dce118f786b6a91a91155926193eaf97a8fe665a
John Knotts <jhnnyk@gmail.com>
2024-06-17 11:28:13 -0600

    adding a matched property to cards

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import './App.css';
 import SingleCard from './components/SingleCard';
 
 const cardImages = [
-  { "src": "../img/helmet-1.png" },
-  { "src": "../img/potion-1.png" },
-  { "src": "../img/ring-1.png" },
-  { "src": "../img/scroll-1.png" },
-  { "src": "../img/shield-1.png" },
-  { "src": "../img/sword-1.png" }
+  { "src": "../img/helmet-1.png", matched: false },
+  { "src": "../img/potion-1.png", matched: false },
+  { "src": "../img/ring-1.png", matched: false },
+  { "src": "../img/scroll-1.png", matched: false },
+  { "src": "../img/shield-1.png", matched: false },
+  { "src": "../img/sword-1.png", matched: false }
 ]
 
 function App() {
@@ -39,16 +39,26 @@ function App() {
   useEffect(() => {
     if (choiceOne && choiceTwo) {
       if (choiceOne.src === choiceTwo.src) {
-        console.log('those cards match!')
+        setCards(prevCards => {
+          return prevCards.map(card => {
+            if (card.src === choiceOne.src) {
+              return {...card, matched: true}
+            } else {
+              return card
+            }
+          })
+        })
         resetTurn()
       } else {
-        console.log('those cards do NOT match')
+        
         resetTurn()
       }
     }
   }, [choiceOne, choiceTwo])
   // useEffect hook runs once when the component is created
   // and then again if any dependencies (in the array) change
+
+  console.log(cards)
 
   // reset choices & increase turn
   const resetTurn = () => {
