46e4b0d2e754b9566480e05bdefcd9fbfa0b0d66
John Knotts <jhnnyk@gmail.com>
2024-06-17 10:39:29 -0600

    SingleCard component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { useState } from 'react';
 import './App.css';
+import SingleCard from './components/SingleCard';
 
 const cardImages = [
-  { "src": "/img/helmet-1.png" },
-  { "src": "/img/potion-1.png" },
-  { "src": "/img/ring-1.png" },
-  { "src": "/img/scroll-1.png" },
-  { "src": "/img/shield-1.png" },
-  { "src": "/img/sword-1.png" }
+  { "src": "../img/helmet-1.png" },
+  { "src": "../img/potion-1.png" },
+  { "src": "../img/ring-1.png" },
+  { "src": "../img/scroll-1.png" },
+  { "src": "../img/shield-1.png" },
+  { "src": "../img/sword-1.png" }
 ]
 
 function App() {
@@ -32,12 +33,7 @@ function App() {
       <button onClick={shuffleCards}>New Game</button>
       <div className="card-grid">
         {cards.map(card => (
-          <div className='card' key={card.id}>
-            <div>
-              <img src={card.src} className='front' alt="card front" />
-              <img src="/img/cover.png" className='back' alt="card back" />
-            </div>
-          </div>
+          <SingleCard key={card.id} card={card} />
         ))}
       </div>
     </div>
